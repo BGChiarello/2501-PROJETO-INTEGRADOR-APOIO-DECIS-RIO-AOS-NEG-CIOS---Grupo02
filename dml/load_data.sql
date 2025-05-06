93b2f2dbb1ec62652d615a38952bb4d0c11dea2b
Stefani Dal Puppo <107705645+stefanipuppo@users.noreply.github.com>
2025-05-06 11:56:34 -0300

    Delete dml/load_data.sql

--- dml/load_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO populacao (ano, regiao, faixa_etaria, sexo, populacao) VALUES
-(2024, 'Brasil', 'Bebês (0 a 2 anos)', 'Feminino', 4123665),
-(2024, 'Brasil', 'Bebês (0 a 2 anos)', 'Masculino', 4005623),
-(2024, 'Brasil', '1ª Infância (3 a 4 anos)', 'Feminino', 2991288),
-(2024, 'Brasil', '1ª Infância (3 a 4 anos)', 'Masculino', 2979804),
-(2024, 'Brasil', '2ª Infância (5 a 10 anos)', 'Feminino', 9140838),
-(2024, 'Brasil', '2ª Infância (5 a 10 anos)', 'Masculino', 9104998);
