ee58902cd3fe1f05cffdb51f47082a36ec2fd3a8
Stefani Dal Puppo <107705645+stefanipuppo@users.noreply.github.com>
2025-05-06 11:53:27 -0300

    Update create_tables.sql

--- ddl/create_tables.sql
+++ ddl/create_tables.sql
@@ -1,11 +1,2 @@
 CREATE DATABASE IF NOT EXISTS proj_populacional;
 USE proj_populacional;
-
-CREATE TABLE populacao (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    ano INT NOT NULL,
-    regiao VARCHAR(30),
-    faixa_etaria VARCHAR(50),
-    sexo ENUM('Masculino', 'Feminino'),
-    populacao BIGINT
-);
